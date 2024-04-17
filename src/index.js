5dc5d0122b49c5ced606df2ccc2fd58645730f04
Lucia Struth <l.struth@icgc.cat>
2024-04-17 08:47:17 +0200

    export constants before map init

--- src/index.js
+++ src/index.js
@@ -1,39 +1,33 @@
 import Map from "./map/Map";
 import CompareMaps from "./plugins/CompareMaps";
-// import Styles from "./constants/Styles";
-// import Terrains from "./constants/Terrains";
-// import Layers from "./constants/Layers";
 import axios from "axios";
-import mapicgcConfig from "./mapicgc-config.json"
+import mapicgcConfig from "./mapicgc-config.json";
 
 
+export let Styles = axios
+  .get("https://tilemaps.icgc.cat/cdn/configs/mapicgc-config.json")
+  .then((response) => {
+    Styles = response.data.Styles;
+  })
+  .catch((error) => {
+    Styles = mapicgcConfig.Styles;
+  });
+export let Terrains = axios
+  .get("https://tilemaps.icgc.cat/cdn/configs/mapicgc-config.json")
+  .then((response) => {
+    Terrains = response.data.Terrains;
+  })
+  .catch((error) => {
+    Terrains = mapicgcConfig.Terrains;
+  });
 
-let Layers 
-let Styles 
-let Terrains 
+export let Layers = axios
+  .get("https://tilemaps.icgc.cat/cdn/configs/mapicgc-config.json")
+  .then((response) => {
+    Layers = response.data.Layers;
+  })
+  .catch((error) => {
+    Layers = mapicgcConfig.Layers;
+  });
 
-axios.get('https://tilemaps.icgc.cat/cdn/configs/mapicgc-config.json')
-    .then (response =>{
-      // console.log('entro')
-      Layers = response.data.Layers;
-      Styles = response.data.Styles;
-      Terrains = response.data.Terrains;
-
-      // console.log('response', response.data)
-	})
-	.catch(error =>{
-	   Layers = mapicgcConfig.Layers;
-       Styles = mapicgcConfig.Styles;
-       Terrains = mapicgcConfig.Terrains;
-
-	});
-
-
-
-export {
-	Map, 
-	Styles,
-	Terrains,
-	Layers,
-	CompareMaps
-};
+export { Map, CompareMaps };
